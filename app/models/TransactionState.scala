fcc00f53aae6bc1c6070080781f9961706a62a48
noob77777 <abhishekpal7@outlook.com>
2023-05-05 23:50:36 +0530

    :add: TransactionState model

---
+++ app/models/TransactionState.scala
@@ -0,0 +1,34 @@
+package models
+
+import play.api.libs.json.Json
+import play.api.libs.json.Reads
+import play.api.libs.json.Writes
+
+import slick.jdbc.PostgresProfile.api._
+
+import java.util.UUID
+
+object TransactionStateStatus extends Enumeration {
+  type TransactionStateStatus = Value
+  val SUBMITTED, PENDING, CONFIRMED, FAILED = Value
+
+  implicit val readsTransactionStateStatus =
+    Reads.enumNameReads(TransactionStateStatus)
+  implicit val writesTransacionStateStatus = Writes.enumNameWrites
+  implicit val statusMapper =
+    MappedColumnType.base[TransactionStateStatus, String](
+      e => e.toString,
+      s => TransactionStateStatus.withName(s)
+    )
+}
+
+final case class TransactionState(
+    id: UUID,
+    transactionId: String,
+    status: TransactionStateStatus.Value,
+    retryCount: Int
+)
+
+object TransactionState {
+  implicit val json = Json.format[TransactionState]
+}
