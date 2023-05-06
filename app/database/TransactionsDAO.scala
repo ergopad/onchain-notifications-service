8f19d3d9302259bc2aafda178d8848a67b48d0e8
noob77777 <abhishekpal7@outlook.com>
2023-05-06 22:08:21 +0530

    :add: TransactionsDAO

---
+++ app/database/TransactionsDAO.scala
@@ -0,0 +1,112 @@
+package database
+
+import java.util.UUID
+
+import javax.inject.Inject
+import javax.inject.Singleton
+
+import play.api.db.slick.DatabaseConfigProvider
+import play.api.db.slick.HasDatabaseConfigProvider
+import play.api.Logging
+
+import scala.concurrent.ExecutionContext
+import scala.concurrent.Future
+
+import slick.jdbc.JdbcProfile
+import database.JsonPostgresProfile.api._
+
+import models._
+
+@Singleton
+class UsersDAO @Inject() (
+    protected val dbConfigProvider: DatabaseConfigProvider
+)(implicit
+    ec: ExecutionContext
+) extends HasDatabaseConfigProvider[JdbcProfile]
+    with Logging {
+
+  def getAll: Future[Seq[TransactionState]] = {
+    val query = TransactionStates.transactionStates.result
+    db.run(query)
+  }
+
+  def getTransactionStateById(id: UUID): Future[Option[TransactionState]] = {
+    val query =
+      TransactionStates.transactionStates.filter(_.id === id).result.headOption
+    db.run(query)
+  }
+
+  def getPendingTransactions: Future[Seq[TransactionState]] = {
+    val query = TransactionStates.transactionStates
+      .filter(_.status === TransactionStateStatus.PENDING)
+      .result
+    db.run(query)
+  }
+
+  def getTransaction(
+      transactionId: String
+  ): Future[Option[TransactionState]] = {
+    val query = TransactionStates.transactionStates
+      .filter(_.transactionId === transactionId)
+      .result
+      .headOption
+    db.run(query)
+  }
+
+  def insertTransactionState(
+      transactionState: TransactionState
+  ): Future[Any] = {
+    val query = TransactionStates.transactionStates += transactionState
+    db.run(query)
+  }
+
+  def updateTransactionState(
+      transactionState: TransactionState
+  ): Future[Any] = {
+    val query =
+      for (
+        dbTransactionState <- TransactionStates.transactionStates
+        if dbTransactionState.id === transactionState.id
+      )
+        yield (
+          dbTransactionState.transactionId,
+          dbTransactionState.status,
+          dbTransactionState.retryCount
+        )
+    db.run(
+      query.update(
+        transactionState.transactionId,
+        transactionState.status,
+        transactionState.retryCount
+      )
+    ) map { _ > 0 }
+  }
+
+  def deleteTransaction(id: UUID): Future[Any] = {
+    val query = TransactionStates.transactionStates.filter(_.id === id).delete
+    db.run(query)
+  }
+}
+
+object TransactionStates {
+  class TransactionStates(tag: Tag)
+      extends Table[TransactionState](tag, "transaction_states") {
+    def id = column[UUID]("id", O.PrimaryKey)
+    def transactionId = column[String]("transaction_id")
+    def status = column[TransactionStateStatus.Value]("status")
+    def retryCount = column[Int]("retry_count")
+    def * = (
+      id,
+      transactionId,
+      status,
+      retryCount
+    ) <> ((TransactionState.apply _).tupled, TransactionState.unapply)
+    def transactionIdIndex = index(
+      "transaction_states_transaction_id_index",
+      (transactionId),
+      unique = true
+    )
+  }
+
+  val transactionStates = TableQuery[TransactionStates]
+}
--- app/tasks/StartupTask.scala
+++ app/tasks/StartupTask.scala
@@ -7,10 +7,15 @@ import play.api.db.slick.HasDatabaseConfigProvider
 
 import javax.inject.Inject
 
+import scala.concurrent.Await
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 
+import slick.basic.DatabasePublisher
 import slick.jdbc.JdbcProfile
+import slick.jdbc.PostgresProfile.api._
+
+import database._
 
 class StartupTask @Inject() (
     protected val dbConfigProvider: DatabaseConfigProvider,
@@ -18,8 +23,13 @@ class StartupTask @Inject() (
 ) extends HasDatabaseConfigProvider[JdbcProfile] {
   actorSystem.scheduler.scheduleOnce(delay = 5.seconds)(
     try {
-      println("Running startup")
-      println("Startup done")
+      println("Running Startup")
+      val schema = TableQuery[TransactionStates.TransactionStates].schema
+      Await.result(
+        db.run(DBIO.seq(schema.create)),
+        Duration.Inf
+      )
+      println("Startup Done")
     } catch {
       case e: Exception => println(e)
     }
